621b44b8953a47f7f8d4215affe7eb562bca8b97
Alamingit247 <68566244+Alamingit247@users.noreply.github.com>
2021-01-20 23:50:33 +0600

    Add files via upload

--- 17. Uri 1961- Jumping Frog.c
+++ 17. Uri 1961- Jumping Frog.c
@@ -1,41 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int h, p, i, dis=0, flag=0;
+    int h, p, i, j[20], dis=0, flag=0;
     scanf("%d %d", &h, &p);
-    int j[p];
-    if((h>=1 && h<=5) && (p>=2 && p<=100))
+
+    for(i=0; i<p; i++)
     {
-        for(i=0; i<p; i++)
+        scanf("%d", &j[i]);
+    }
+    for(i=0; i<p-1; i++)
+    {
+        if(j[i+1]>j[i])
         {
-            scanf("%d", &j[i]);
-        }
-        for(i=1; i<p; i++)
-        {
-            if(j[i]>j[i-1])
-            {
-                dis = (j[i]-j[i-1]);
-            }
-            else
-            {
-                dis = (j[i-1]-j[i]);
-            }
-            if(dis<=h)
-            {
-                flag++;
-            }
-        }
-        if(flag == p-1)
-        {
-            printf("YOU WIN\n");
+            dis = (j[i+1]-j[i]);
         }
         else
         {
-            printf("GAME OVER\n");
+            dis = (j[i]-j[i+1]);
+        }
+        if(dis<=h)
+        {
+            flag++;
         }
     }
-
-
+    if(flag == p-1)
+    {
+        printf("YOU WIN\n");
+    }
+    else
+    {
+        printf("GAME OVER\n");
+    }
     return 0;
 
 }
